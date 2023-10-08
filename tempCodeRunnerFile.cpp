4dcd57b4708ecfbbe863084e9c6143e6dbf0209c
Mahek <114944166+mmahek@users.noreply.github.com>
2023-10-08 19:39:26 +0530

    Lecture 5

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+int array[100][100];
+
+    // cout << "NEW ARRAY: " << endl;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout << "Enter the value of array at position " << i << " , " << j << " : ";
+    //         cin >> array[i][j];
+    //         //cout << array[i][j];
+    //     }
+    //     cout << endl;
+    // }
+    // cout << "NEW ARRAY: " << endl;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout << array[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
